acc3ee7b8985e0718755ce435d3ffb286b69b199
s2hmuel <s2hmuel@gmail.com>
2023-11-26 20:32:10 +0900

    2-10 completed

--- src/main/java/com/ll/sbbmission/question/service/QuestionService.java
+++ src/main/java/com/ll/sbbmission/question/service/QuestionService.java
@@ -1,19 +1,32 @@
 package com.ll.sbbmission.question.service;
 
+import com.ll.sbbmission.exception.DataNotFoundException;
 import com.ll.sbbmission.question.entity.Question;
 import com.ll.sbbmission.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
-
     public List<Question> getList() {
         return this.questionRepository.findAll();
     }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+
 }
--- src/test/java/com/ll/sbbmission/question/service/QuestionServiceTest.java
+++ src/test/java/com/ll/sbbmission/question/service/QuestionServiceTest.java
@@ -1,5 +1,6 @@
 package com.ll.sbbmission.question.service;
 
+import com.ll.sbbmission.exception.DataNotFoundException;
 import com.ll.sbbmission.question.entity.Question;
 import com.ll.sbbmission.question.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class QuestionServiceTest {
@@ -34,7 +37,7 @@ public class QuestionServiceTest {
         List<Question> questionList = new ArrayList<>();
         questionList.add(question);
 
-        Mockito.when(questionRepository.findAll()).thenReturn(questionList);
+        when(questionRepository.findAll()).thenReturn(questionList);
         // QuestionService의 getList 메서드 호출
         List<Question> result = questionService.getList();
 
@@ -42,4 +45,37 @@ public class QuestionServiceTest {
         assertEquals("Question1", result.get(0).getSubject());
         assertEquals("Content1", result.get(0).getContent());
     }
+
+    @Test
+    public void testQuestionFound(){
+        Question question = new Question();
+        question.setId(1);
+        question.setSubject("Question 1");
+        question.setContent("Content 1");
+
+        // Mock Repository의 findById 메서드가 호출될 때 반환할 데이터 설정
+        when(questionRepository.findById(1)).thenReturn(Optional.of(question));
+
+        // QuestionService의 getQuestion 메서드 호출
+        Question result = questionService.getQuestion(1);
+
+        // 결과 확인
+        assertEquals(1, result.getId());
+        assertEquals("Question 1", result.getSubject());
+        assertEquals("Content 1", result.getContent());
+    }
+
+    @Test
+    public void testQuestionNotFound() {
+        // Mock Repository의 findById 메서드가 호출될 때 반환할 데이터 설정
+        when(questionRepository.findById(1)).thenReturn(Optional.empty());
+
+        // 특정 ID에 해당하는 Question이 없을 때, DataNotFoundException이 발생하는지 확인
+        assertThrows(DataNotFoundException.class, () -> {
+            questionService.getQuestion(1);
+        });
+    }
+
+
+
 }
